dd1340f6749a5e69d3314302224b71c94a95d184
Roberto Espinosa <roberto.espinosa@btob.com.mx>
2020-07-26 21:25:18 -0500

    Docs agregado

---
+++ docs/precache-manifest.ba97eec9e5221b4fdda1b01caccfe04f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b95fc58eb2667e952b6d7f34a117cc51",
+    "url": "/index.html"
+  },
+  {
+    "revision": "99e5938f91b78e11e849",
+    "url": "/static/css/main.a17e7587.chunk.css"
+  },
+  {
+    "revision": "ecc4420cc57be6001b77",
+    "url": "/static/js/2.cba4c4e4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.cba4c4e4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "99e5938f91b78e11e849",
+    "url": "/static/js/main.24770fd5.chunk.js"
+  },
+  {
+    "revision": "f1c04d0ff1c1e1d1be53",
+    "url": "/static/js/runtime-main.449490d0.js"
+  }
+]);
